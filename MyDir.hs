db1d6f0136e5c4aca5ad0dbc788914b1a3824b9c
Douglas Burke <dburke@cfa.harvard.edu>
2009-12-29 15:41:28 -0500

    Now exclude . files by default unless use the -a flag

--- MyDir.hs
+++ MyDir.hs
@@ -21,7 +21,7 @@ import Control.Concurrent
 import Control.Monad
 import qualified Control.Exception as C
 
-import Data.Maybe (fromJust, isJust)
+import Data.Maybe (isJust)
 import qualified Data.List as L
 
 -- Having trouble compiling against split-0.1.2 so doing it manually
@@ -42,17 +42,19 @@ data DirContents = DirContents {
     }
                    deriving (Eq, Show)
 
+emptyDir :: DirContents
 emptyDir = DirContents [] [] [] []
 
 -- I don't think that sorting is needed
 -- since getDirectoryContents appears to return a
 -- sorted list, but include for now to make sure
 --
-sortDC (DirContents ds ls xs fs) = DirContents
-                                   (L.sort ds)
-                                   (L.sort ls)
-                                   (L.sort xs)
-                                   (L.sort fs)
+sortDC :: DirContents -> DirContents
+sortDC (DirContents ds lks xs fs) = DirContents
+                                    (L.sort ds)
+                                    (L.sort lks)
+                                    (L.sort xs)
+                                    (L.sort fs)
 
 -- | Return the type of a file, or Nothing if there
 -- was an error accessing the information.
@@ -84,17 +86,23 @@ ls dname = E.try ls'
 
         return ys
 
--- This step also removes "." and ".." from directories list
+-- This step always removes "." and ".." from directories list
+-- and will remove all other entries beginning with "." if 
+-- flag is True
 --                
-lsToDir :: [(FilePath, FileType)] -> DirContents
-lsToDir = sortDC . foldr conv emptyDir
+lsToDir :: Bool -> [(FilePath, FileType)] -> DirContents
+lsToDir flag = sortDC . foldr conv emptyDir
     where
-      conv (".",Directory) inDir = inDir
+      conv (".",Directory) inDir  = inDir
       conv ("..",Directory) inDir = inDir
-      conv (fp,Directory)  inDir = inDir { dirs = fp : dirs inDir }
-      conv (fp,Link)       inDir = inDir { links = fp : links inDir }
-      conv (fp,Executable) inDir = inDir { executables = fp : executables inDir }
-      conv (fp,File)       inDir = inDir { files = fp : files inDir }
+      conv (fp,Directory)  inDir | flag && head fp == '.' = inDir
+                                 | otherwise              = inDir { dirs = fp : dirs inDir }
+      conv (fp,Link)       inDir | flag && head fp == '.' = inDir
+                                 | otherwise              = inDir { links = fp : links inDir }
+      conv (fp,Executable) inDir | flag && head fp == '.' = inDir
+                                 | otherwise              = inDir { executables = fp : executables inDir }
+      conv (fp,File)       inDir | flag && head fp == '.' = inDir
+                                 | otherwise              = inDir { files = fp : files inDir }
 
 -- | Create the column list of a set of files. Each file is limited to
 -- either n or n-1 characters in length (if lastChar is Nothing
@@ -134,22 +142,6 @@ columnToLine n = map (L.intercalate " ") . chunk n
 
 -- | get the number of characters in the current terminal.
 -- 
--- The current approach is not ideal, and doesn't work from here
--- since need to set up stdin for /bin/stty properly. Argh.
---
-{-
-terminalWidth :: IO Int
-terminalWidth = do
-  (ecode,out,err) <- readProcessWithExitCode "/bin/stty" ["size"] []
-  putStrLn $ "ECODE == " ++ show ecode
-  putStrLn $ "OUT == " ++ out
-  putStrLn $ "ERR == " ++ err
-  if ecode == ExitSuccess
-    then (return . read . tail . dropWhile (/=' ')) out
-    else return 80
--}
-
--- based on readProcess
 terminalWidth :: IO Int
 terminalWidth = do
   (_, Just outh, _, pid) <-
@@ -176,14 +168,15 @@ terminalWidth = do
 
 
 listContents :: Int -- ^ terminal width in characters
-             -> FilePath -- ^ directory to lost
+             -> Bool -- ^ True if files beginning with "." should be included
+             -> FilePath -- ^ directory to list
              -> IO ()
-listContents tw dname = do
+listContents tw flag dname = do
   cnts <- ls dname
   case cnts of
     Left eval   -> putStrLn ("Unable to access directory <" ++ dname ++ ">:\n" ++ show eval) >> exitFailure
     Right cnts' -> do
-              let dcnts = lsToDir cnts'
+              let dcnts = lsToDir flag cnts'
                   ncols = 4
                   n = (tw - ncols) `div` ncols
                   dnames = mkColumn n (Just '/') (dirs dcnts)
@@ -196,16 +189,25 @@ listContents tw dname = do
               unless (null fnames) $ mapM_ putStrLn (columnToLine ncols fnames)
               exitSuccess
 
+usage :: IO ()
 usage = getProgName >>= \n -> 
         putStrLn ("Usage: " ++ n ++ " [-a] [directory]") >> 
         exitFailure
 
+main :: IO ()
 main = do
   args <- getArgs
   tWidth <- terminalWidth
-  case length args of
-    0 -> listContents tWidth "."
-    1 -> listContents tWidth (head args)
-    _ -> usage
+  if null args
+    then listContents tWidth True "."
+    else if length args > 2
+       then usage
+       else do
+         let flag = head args == "-a"
+             dname = if length args == 2 then (head . tail) args else if flag then "." else head args
+         when (length args == 2 && not flag) usage
+         listContents tWidth (not flag) dname
+         
+
 
 
