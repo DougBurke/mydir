2a1ebd55308434671a94856b91f4b3dbe27249df
Douglas Burke <dburke.gw@gmail.com>
2022-09-05 11:48:33 -0400

    flake it up, based on Serokell blog

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  # inspired by: https://serokell.io/blog/practical-nix-flakes#packaging-existing-applications
+  description = "An opinionated directory listing tool";
+  inputs.nixpkgs.url = "nixpkgs";
+  outputs = { self, nixpkgs }:
+    let
+      # supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
+      });
+    in
+    {
+      overlay = (final: prev: {
+        mydir = final.haskellPackages.callCabal2nix "mydir" ./. {};
+      });
+      packages = forAllSystems (system: {
+         mydir = nixpkgsFor.${system}.mydir;
+      });
+      defaultPackage = forAllSystems (system: self.packages.${system}.mydir);
+      checks = self.packages;
+      devShell = forAllSystems (system: let haskellPackages = nixpkgsFor.${system}.haskellPackages;
+        in haskellPackages.shellFor {
+          packages = p: [self.packages.${system}.mydir];
+          # withHoogle = true;
+          buildInputs = with haskellPackages; [
+            # haskell-language-server
+	    ghcid
+            hlint
+            cabal-install
+          ];
+        # Change the prompt to show that you are in a devShell
+        shellHook = "export PS1='\\e[1;34mdev[mydir] > \\e[0m'";
+        });
+  };
+}
